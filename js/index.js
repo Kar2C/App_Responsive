ca1f0f81ac07497881573a67cb65368ffe63106c
Kar2C <kycarrillo@uniboyaca.edu.co>
2024-08-28 22:04:07 -0500

    Se hicieron cambios en el diseño de la app. De igual modo se agregaron las carpetas correspondientes para cada uno de los archivos y se esta empezando a integrar el contenido de typescript

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('typescript-button').addEventListener('click', function() {
+        window.location.href = 'typescript.html'; // Ruta a la página de TypeScript
+    });
+
+    document.getElementById('angular-button').addEventListener('click', function() {
+        window.location.href = 'angular.html'; // Ruta a la página de Angular
+    });
+
+    document.getElementById('ionic-button').addEventListener('click', function() {
+        window.location.href = 'ionic.html'; // Ruta a la página de Ionic
+    });
+});
