54eb0d2eb1a15ccbec9fac318882d82cac538038
Kar2C <kycarrillo@uniboyaca.edu.co>
2024-09-02 22:18:20 -0500

    Se hciieron los ultimos cambios en el diseño de la aplicación y se esta terminando de implementar la información correspondiente

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('typescript-button').addEventListener('click', function() {
-        window.location.href = 'typescript.html'; // Ruta a la página de TypeScript
+        window.location.href = 'typescript.html'; // Ruta a TypeScript
     });
 
     document.getElementById('angular-button').addEventListener('click', function() {
-        window.location.href = 'angular.html'; // Ruta a la página de Angular
+        window.location.href = 'angular.html'; // Ruta a Angular
     });
 
     document.getElementById('ionic-button').addEventListener('click', function() {
-        window.location.href = 'ionic.html'; // Ruta a la página de Ionic
+        window.location.href = 'ionic.html'; // Ruta a Ionic
     });
 
     document.getElementById('home-button').addEventListener('click', function() {
-        window.location.href = 'index.html'; // Ruta a la página de index
+        window.location.href = 'index.html'; // Ruta a index
     });
 });
